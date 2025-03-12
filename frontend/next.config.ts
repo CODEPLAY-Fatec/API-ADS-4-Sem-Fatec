265e04b8ec31f93fa05112257d31e15b4922eb9f
gabrielvascf <93106982+gabrielvascf@users.noreply.github.com>
2025-03-11 22:53:51 -0300

    Adicionar proxy para direcionar requests para o servidor ativo, e não localhost.

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*", // All requests to `/api/*` will be redirected
+        destination: "http://localhost:3001/api/:path*", // Proxy requests to port 3001
+      },
+    ];
+  },
 };
 
 export default nextConfig;
