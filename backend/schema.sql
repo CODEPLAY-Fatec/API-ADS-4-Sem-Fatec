c47c1a233ca79549063101bbcd073f1898385d5e
gabrielvascf <93106982+gabrielvascf@users.noreply.github.com>
2025-03-14 11:31:57 -0300

    Atualizar estrutura do banco de dados para permitir conexões fracas

--- backend/schema.sql
+++ backend/schema.sql
@@ -21,7 +21,7 @@ CREATE TABLE `projects` (
   `subject` varchar(255),
   `institution` varchar(255),
   `creator` int NOT NULL,
-  `status` ENUM ('Fechado', 'Em andamento', 'Concluído')
+  `status` ENUM ('Fechado', 'Em andamento', 'Concluído') DEFAULT 'Fechado'
 );
 
 CREATE TABLE `projectSubjects` (
@@ -44,7 +44,7 @@ CREATE TABLE `tasks` (
   `timeEstimate` int COMMENT 'em horas',
   `start` datetime,
   `finish` datetime,
-  `status` ENUM ('Fechado', 'Em andamento', 'Concluído') NOT NULL
+  `status` ENUM ('Fechado', 'Em andamento', 'Concluído') NOT NULL DEFAULT 'Fechado'
 );
 
 CREATE TABLE `projectRecovery` (
@@ -73,18 +73,22 @@ CREATE TABLE `taskRecovery` (
   `created` datetime NOT NULL
 );
 
-ALTER TABLE `userPicture` ADD FOREIGN KEY (`userId`) REFERENCES `users` (`id`);
+ALTER TABLE `userPicture` ADD FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 
-ALTER TABLE `projects` ADD FOREIGN KEY (`subject`) REFERENCES `projectSubjects` (`name`);
+ALTER TABLE `projects` ADD FOREIGN KEY (`subject`) REFERENCES `projectSubjects` (`name`) ON DELETE SET NULL;
 
-ALTER TABLE `projects` ADD FOREIGN KEY (`institution`) REFERENCES `institutions` (`name`);
+ALTER TABLE `projects` ADD FOREIGN KEY (`institution`) REFERENCES `institutions` (`name`) ON DELETE SET NULL;
 
-ALTER TABLE `projects` ADD FOREIGN KEY (`creator`) REFERENCES `users` (`id`);
+ALTER TABLE `projects` ADD FOREIGN KEY (`creator`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 
-ALTER TABLE `tasks` ADD FOREIGN KEY (`projectId`) REFERENCES `projects` (`id`);
+ALTER TABLE `tasks` ADD FOREIGN KEY (`projectId`) REFERENCES `projects` (`id`) ON DELETE CASCADE;
 
-ALTER TABLE `tasks` ADD FOREIGN KEY (`taskUser`) REFERENCES `users` (`id`);
+ALTER TABLE `tasks` ADD FOREIGN KEY (`taskUser`) REFERENCES `users` (`id`) ON DELETE SET NULL;
 
-ALTER TABLE `projectRecovery` ADD FOREIGN KEY (`projectId`) REFERENCES `projects` (`id`);
+ALTER TABLE `projectRecovery` ADD FOREIGN KEY (`projectId`) REFERENCES `projects` (`id`) ON DELETE CASCADE;
 
-ALTER TABLE `taskRecovery` ADD FOREIGN KEY (`taskId`) REFERENCES `tasks` (`id`);
+ALTER TABLE `taskRecovery` ADD FOREIGN KEY (`taskId`) REFERENCES `tasks` (`id`) ON DELETE CASCADE;
+
+INSERT INTO projectSubjects (name) VALUES ('Exatas')
+
+INSERT INTO institutions (name) VALUES ('Fatec')
