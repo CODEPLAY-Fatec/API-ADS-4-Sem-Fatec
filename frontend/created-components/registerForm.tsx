f60e53c53450952f55f035d915537fdcf0b01d2c
Gabriel <143124341+gabrielfelip@users.noreply.github.com>
2025-03-14 10:41:11 -0300

    mensagem de feedback implementado

--- frontend/created-components/registerForm.tsx
+++ frontend/created-components/registerForm.tsx
@@ -2,12 +2,11 @@
 import axios from "axios";
 import { User } from "@shared/User";
 import React, { useState } from "react";
-import { useRouter } from "next/navigation";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
-import { Label } from "@/components/ui/label"; 
-import GradientText from "./GradientText";
-import FadeIn from "./FadeIn";
-import EmergeIn from "./EmergeIn";
+import { useRouter } from "next/navigation"; // Importe o useRouter para redirecionar
+import toast from "react-hot-toast"; // Importe o toast para exibir mensagens
 
 const RegisterForm: React.FC = () => {
     const router = useRouter();
@@ -21,7 +20,7 @@ const RegisterForm: React.FC = () => {
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const { name, value } = e.target;
-        setFormData((prevData) => ({
+        setFormData((prevData: User) => ({
             ...prevData,
             [name]: value,
         }));
@@ -29,15 +28,42 @@ const RegisterForm: React.FC = () => {
 
     const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
+
+        // Validação dos campos
+        if (!formData.name || !formData.email || !formData.phoneNumber || !formData.password) {
+            toast.error("Por favor, preencha todos os campos.");
+            return;
+        }
+
         setLoading(true);
+
         try {
-            await axios.post("/api/users", formData);
-            // Redirecionar para a tela de login após sucesso
-            router.push("/login");
-        } catch (error) {
-            console.error("Erro ao cadastrar o usuário", error);
+            const response = await axios.post("/api/users", formData);
+            if (response.status === 201) {
+                toast.success("Cadastro realizado com sucesso!"); // Mensagem de sucesso
+                router.push("/login"); // Redireciona para a página de login
+            }
+        } catch (error: unknown) {
+            console.error("Erro ao cadastrar o usuário:", error);
+
+            // Verifica o tipo de erro e exibe mensagens específicas
+            if (axios.isAxiosError(error) && error.response) {
+                const { status } = error.response;
+
+                if (status === 400) {
+                    toast.error("Dados inválidos. Verifique os campos e tente novamente.");
+                } else if (status === 409) {
+                    toast.error("E-mail já cadastrado. Use outro e-mail.");
+                } else {
+                    toast.error("Erro ao realizar cadastro. Tente novamente mais tarde.");
+                }
+            } else if (axios.isAxiosError(error) && error.request) {
+                toast.error("Erro de conexão. Verifique sua internet e tente novamente.");
+            } else {
+                toast.error("Erro inesperado. Tente novamente.");
+            }
         } finally {
-            setLoading(false);
+            setLoading(false); // Encerrar o carregamento
             setFormData({
                 name: "",
                 email: "",
@@ -48,102 +74,66 @@ const RegisterForm: React.FC = () => {
     };
 
     return (
-        <EmergeIn>
-                        <div className="flex min-h-screen items-center justify-center px-6">
-                <button
-                    type="button"
-                    onClick={() => router.push("/")}
-                    className="absolute left-4 top-4 p-2"
-                >
-                    <svg
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            stroke="currentColor"
-                            d="M15 19l-7-7 7-7"
-                        />
-                    </svg>
-                </button>
-
-                <div className="w-full max-w-md rounded-2xl bg-white p-8 shadow-[0px_4px_10px_rgba(0,0,0,0.1),_0px_-4px_10px_rgba(0,0,0,0.1)]">
-                    <GradientText>Crie sua conta</GradientText>
-                    <form onSubmit={handleSubmit} className="mt-6 space-y-6">
-                        <div>
-                            <Label htmlFor="name" className="text-gray-700">Nome</Label>
-                            <input
-                                type="text"
-                                id="name"
-                                name="name"
-                                value={formData.name}
-                                onChange={handleChange}
-                                required
-                                className="mt-1 w-full rounded-lg border border-gray-300 p-2"
-                            />
-                        </div>
-
-                        <div>
-                            <Label htmlFor="email" className="text-gray-700">E-mail</Label>
-                            <input
-                                type="email"
-                                id="email"
-                                name="email"
-                                value={formData.email}
-                                onChange={handleChange}
-                                required
-                                className="mt-1 w-full rounded-lg border border-gray-300 p-2"
-                            />
-                        </div>
-
-                        <div>
-                            <Label htmlFor="phoneNumber" className="text-gray-700">Número de telefone</Label>
-                            <input
-                                type="text"
-                                id="phoneNumber"
-                                name="phoneNumber"
-                                value={formData.phoneNumber}
-                                onChange={handleChange}
-                                required
-                                className="mt-1 w-full rounded-lg border border-gray-300 p-2"
-                            />
-                        </div>
-
-                        <div>
-                            <Label htmlFor="password" className="block text-sm font-medium text-gray-700">Senha</Label>
-                            <input
-                                type="password"
-                                id="password"
-                                name="password"
-                                value={formData.password}
-                                onChange={handleChange}
-                                required
-                                className="mt-1 w-full rounded-lg border border-gray-300 p-2"
-                            />
-                        </div>
-
-                        <div className="text-center">
-                            <a href="/login" className="text-sm text-blue-500 hover:underline">
-                                Já tem uma conta? Entre aqui
-                            </a>
-                        </div>
-
-                        <Button
-                            type="submit"
-                            disabled={loading}
-                            className="w-3/4 bg-[#1C3373] text-white hover:bg-[#162b5e] hover:scale-105 mx-auto block rounded-full transition-transform"
-                        >
-                            {loading ? "Cadastrando..." : "Cadastrar"}
-                        </Button>
-                    </form>
+        <div className="max-w-md mx-auto border border-black p-6 rounded-md">
+            <h2 className="text-4xl font-black text-center mb-4">Cadastro</h2>
+            <form onSubmit={handleSubmit} className="space-y-4">
+                <div>
+                    <Label htmlFor="email">Email</Label>
+                    <Input
+                        type="text"
+                        id="email"
+                        name="email"
+                        value={formData.email}
+                        onChange={handleChange}
+                        required
+                    />
                 </div>
-            </div>
-        </EmergeIn>
+
+                <div>
+                    <Label htmlFor="password">Senha</Label>
+                    <Input
+                        type="password"
+                        id="password"
+                        name="password"
+                        value={formData.password}
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+
+                <div>
+                    <Label htmlFor="name">Nome</Label>
+                    <Input
+                        type="text"
+                        id="name"
+                        name="name"
+                        value={formData.name}
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+
+                <div>
+                    <Label htmlFor="phoneNumber">Número de telefone</Label>
+                    <Input
+                        type="text"
+                        id="phoneNumber"
+                        name="phoneNumber"
+                        value={formData.phoneNumber}
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+
+                <Button
+                    type="submit"
+                    disabled={loading}
+                    className="w-3/4 bg-[#1C3373] text-white hover:bg-[#162b5e] hover:scale-105 mx-auto block rounded-full transition-transform"
+                >
+                    {loading ? "Cadastrando..." : "Cadastrar"}
+                </Button>
+            </form>
+        </div>
     );
 };
 
