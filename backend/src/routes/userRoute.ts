ab3c8af7ed8af381b9c174c2232f62be86992a08
gabrielvascf <93106982+gabrielvascf@users.noreply.github.com>
2025-04-02 18:03:26 -0300

    fix: erros de digitação e importação incorreta do esquema do banco de dados

--- backend/src/routes/userRoute.ts
+++ backend/src/routes/userRoute.ts
@@ -1,6 +1,13 @@
 import { Router } from "express";
 import { loginController } from "../controllers/authController";
-import { createUserController, getAllUsersController, logoutController, meController,AttPasswordController,upadateUserController } from "../controllers/userController";
+import {
+  createUserController,
+  getAllUsersController,
+  logoutController,
+  meController,
+  AttPasswordController,
+  upadateUserController,
+} from "../controllers/userController";
 import { authenticateToken } from "../middleware/middleware";
 
 const router: Router = Router();
@@ -9,8 +16,8 @@ router.post("/users", createUserController);
 router.get("/me", meController);
 router.post("/logout", logoutController);
 router.post("/login", loginController);
-router.patch("/users/password",authenticateToken, AttPasswordController);
-router.patch("users",authenticateToken,upadateUserController);//rota para atualizar o usuario logado, é so mandar o user como objeto 
+router.patch("/users/password", authenticateToken, AttPasswordController);
+router.patch("/users", authenticateToken, upadateUserController); //rota para atualizar o usuario logado, é so mandar o user como objeto
 //router.get("/users", getAllUsersController);
 
 export default router;
--- backend/src/services/projectService.ts
+++ backend/src/services/projectService.ts
@@ -1,36 +1,40 @@
 import Project from "@shared/Project";
 import { User } from "@shared/User";
 import { PrismaClient } from "@prisma/client";
-import Task from "@shared/Task"
+import Task from "@shared/Task";
 
 const prisma = new PrismaClient();
-
 
 export const hasAccess = async (projectId: number, userId: number) => {
   return prisma.projects.findFirst({
     select: { id: true },
-    where: { id: projectId, creator: userId }
+    where: { id: projectId, creator: userId },
   });
-}
+};
 
-export const createProjectService = async (projectData: Project, user: User) => {
-
+export const createProjectService = async (
+  projectData: Project,
+  user: User,
+) => {
   const create = await prisma.projects.create({
     data: {
       name: projectData.name,
       description: projectData.description || null,
       subject: projectData.subject || null,
-      status: projectData.status,//so aceita Em_andamento e Concluido sem acento
+      status: projectData.status, //so aceita Em_andamento e Concluido sem acento
       institution: projectData.institution || null,
       creator: user.id!,
       start: projectData.start || new Date(),
       finish: projectData.finish || new Date(),
-    }
-  })
+    },
+  });
   return create;
 };
 
-export const updateProjectService = async (projectData: Project, user: User) => {
+export const updateProjectService = async (
+  projectData: Project,
+  user: User,
+) => {
   const HasAcess = await hasAccess(projectData.id!, user.id!);
   if (!HasAcess) {
     throw new Error("Usuário não tem permissão para editar este projeto.");
@@ -45,53 +49,63 @@ export const updateProjectService = async (projectData: Project, user: User) => 
       description: projectData.description,
       subject: projectData.subject,
       status: projectData.status,
-      institution: projectData.institution
+      institution: projectData.institution,
     },
   });
 
   return updatedProject;
 };
 
-export const addUserToProjectService = async (projectId: number, userId: number, user: User) => {
-  const HasAcess = await hasAccess(projectId, user.id!);
-  if (!hasAccess) {
-    throw new Error("Usuário não tem permissão para adicionar usuários a este projeto.");
+export const addUserToProjectService = async (
+  projectId: number,
+  userId: number,
+  user: User,
+) => {
+  const HasAccess = await hasAccess(projectId, user.id!);
+  if (!HasAccess) {
+    throw new Error(
+      "Usuário não tem permissão para adicionar usuários a este projeto.",
+    );
   }
-  const alredyIn = await prisma.projectmember.findFirst({
+  const alredyIn = await prisma.projectMember.findFirst({
     where: {
       projectId: projectId,
-      userId: userId
-    }
+      userId: userId,
+    },
   });
   if (alredyIn) {
     throw new Error("Usuário já está adicionado a este projeto.");
   }
-  const addMember = await prisma.projectmember.create({
+  const addMember = await prisma.projectMember.create({
     data: {
       projectId: projectId,
-      userId: userId
-    }
+      userId: userId,
+    },
   });
   return addMember;
-}
+};
 
-
-export const removeUserFromProjectService = async (projectId: number, userId: number, user: User) => {
+export const removeUserFromProjectService = async (
+  projectId: number,
+  userId: number,
+  user: User,
+) => {
   const HasAcess = await hasAccess(projectId, user.id!);
   if (!HasAcess) {
-    throw new Error("Usuário não tem permissão para remover usuários deste projeto.");
+    throw new Error(
+      "Usuário não tem permissão para remover usuários deste projeto.",
+    );
   }
-  const removeMember = await prisma.projectmember.delete({
+  const removeMember = await prisma.projectMember.delete({
     where: {
       projectId_userId: {
         projectId: projectId,
-        userId: userId
-      }
-    }
+        userId: userId,
+      },
+    },
   });
   return removeMember;
-
-}
+};
 
 export const getProjectsService = async (user: User) => {
   const projects = await prisma.projects.findMany({
@@ -99,79 +113,79 @@ export const getProjectsService = async (user: User) => {
       OR: [
         { creator: user.id },
         {
-          projectmember: {
+          projectMember: {
             some: {
-              userId: user.id
-            }
-          }
-        }
-      ]
-
+              userId: user.id,
+            },
+          },
+        },
+      ],
     },
     include: {
       users: {
         select: {
           id: true,
           name: true,
-          email: true
-        }
+          email: true,
+        },
       },
-    }
-  })
+    },
+  });
   return projects;
 };
 
 export const getProjectByIdService = async (projectId: number, user: User) => {
+  if (!(await hasAccess(projectId, user.id))) {
+    throw new Error("Usuário não tem permissão para acessar este projeto.");
+  }
   const project = await prisma.projects.findUnique({
     where: {
-      id: projectId
+      id: projectId,
     },
     include: {
-      projectmember: {
+      projectMember: {
         select: {
           users: {
             select: {
               id: true,
               name: true,
               email: true,
-            }
-
-          }
-        }
-      }
-
-    }
-  })
+            },
+          },
+        },
+      },
+    },
+  });
   return project;
 };
 
 export const getProjectSubjectsService = async () => {
-  const projectSubjects = await prisma.projectsubjects.findMany({
+  const projectSubjects = await prisma.projectSubjects.findMany({
     select: {
-      name: true
-    }
-  })
+      name: true,
+    },
+  });
   return projectSubjects;
 };
 
 export const getInstitutionsService = async () => {
   const institutions = prisma.institutions.findMany({
     select: {
-      name: true
-    }
+      name: true,
+    },
   });
-  return institutions
+  return institutions;
 };
 
 export const deleteProjectService = async (projectId: number, user: User) => {
   const HasAcess = await hasAccess(projectId, user.id!);
   if (!HasAcess) {
     throw new Error("Usuário não tem permissão para deletar este projeto.");
-  };
+  }
   const deleteProject = await prisma.projects.delete({
     where: {
-      id: projectId
-    }
+      id: projectId,
+    },
   });
 };
 
@@ -186,42 +200,46 @@ export const createTaskService = async (task: Task, projectId: number) => {
       status: task.status,
       projectId: projectId,
       timeEstimate: task.timeEstimate || null,
-      taskUser: task.taskUser || null
-    }
-  })
+      taskUser: task.taskUser || null,
+    },
+  });
   return createTask;
-
-}
+};
 
 export const addUserToTaskService = async (taskId: number, userId: number) => {
   const addUserTask = await prisma.tasks.update({
     where: {
-      id: taskId
+      id: taskId,
     },
     data: {
-      taskUser: userId || null
-    }
+      taskUser: userId || null,
+    },
   });
   return addUserTask;
-}
+};
 
-export const getTasksService = async (projectId: number) => {//talvez fazer uma verificaçao de acessso aqui
+export const getTasksService = async (projectId: number) => {
+  //talvez fazer uma verificaçao de acessso aqui
   const tasks = await prisma.tasks.findMany({
     where: {
-      projectId: projectId
-    }
+      projectId: projectId,
+    },
   });
   return tasks;
-}
+};
 
-export const updateTaskService = async (task: Task,user:User,projectId:number) => {
+export const updateTaskService = async (
+  task: Task,
+  user: User,
+  projectId: number,
+) => {
   const HasAccess = await hasAccess(projectId, user.id!);
   if (!HasAccess) {
     throw new Error("Usuário não tem permissão para editar esta tarefa.");
   }
   const updatedTask = await prisma.tasks.update({
     where: {
-      id: task.id
+      id: task.id,
     },
     data: {
       title: task.title,
@@ -230,18 +248,17 @@ export const updateTaskService = async (task: Task,user:User,projectId:number) =
       finish: task.finish,
       priority: task.priority,
       status: task.status,
-      timeEstimate: task.timeEstimate || null
-    }
+      timeEstimate: task.timeEstimate || null,
+    },
   });
-}
+};
 
-export const deleteTaskService = async (taskId:number) => {//delete de task nao precisa ser o criador para deletar
+export const deleteTaskService = async (taskId: number) => {
+  //delete de task nao precisa ser o criador para deletar
   const deleteTask = await prisma.tasks.delete({
     where: {
-      id: taskId
-    }
-  })
+      id: taskId,
+    },
+  });
   return deleteTask;
-}
-
-
+};
