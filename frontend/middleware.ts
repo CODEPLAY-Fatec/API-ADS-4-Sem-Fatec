c135d3e0eccaa3455d5f8cbc20b9a51c33fe6a47
Gabriecarvalho <gabrielcarvalho8@hotmail.com.br>
2025-05-19 13:21:36 -0300

    Fix: Correçao no middleware do frontend
    Corrigido o middleware do frontend que nao estava redirecionando corretamente se voce estivesse logado

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -2,13 +2,13 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 const protectedRoutes = ["/projetos"];//rotas que precisam de autenticação
-const authCheckRoutes = ["/","/cadastro", "/login"];//rotas que não precisam de autenticação
+const authCheckRoutes = ["/", "/cadastro", "/login"];//rotas que não precisam de autenticação
 
 export async function middleware(request: NextRequest) {
   const pathname = request.nextUrl.pathname;
 
   try {
-    await new Promise((resolve) => setTimeout(resolve, 500)); 
+    await new Promise((resolve) => setTimeout(resolve, 250));
 
     const res = await fetch(`${request.nextUrl.origin}/api/me`, {
       method: "GET",
@@ -16,20 +16,25 @@ export async function middleware(request: NextRequest) {
       headers: { Cookie: request.headers.get("cookie") || "" },//pega desse jeito diferente pq aqui roda no servidor
     });
 
+
     if (!res.ok) {
-      throw new Error("Usuário não autenticado.");
+      console.log("Usuário não autenticado.");
+      if (protectedRoutes.some(route => pathname.startsWith(route))) {
+        return NextResponse.redirect(new URL("/", request.url));
+      }
+      return NextResponse.next();
     }
 
     console.log("Usuário autenticado!");
 
-    
+
     if (authCheckRoutes.includes(pathname)) {//redirecionamentos do login/home/cadastro
       return NextResponse.redirect(new URL("/projetos", request.url));
     }
   } catch (error) {
     console.error("Usuário não autenticado!", error);
 
-    
+
     if (protectedRoutes.some(route => pathname.startsWith(route))) {
       return NextResponse.redirect(new URL("/", request.url));
     }
@@ -39,5 +44,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/", "/projetos/:path*"], // rotas em q o arquivo esta aplicado
+  matcher: ["/", "/projetos/:path*", "/login", "/cadastro"], // rotas em q o arquivo esta aplicado
 };
